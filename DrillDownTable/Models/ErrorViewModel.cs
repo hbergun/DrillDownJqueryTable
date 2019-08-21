e0d858ec6c4a81e8c979accac69cabfb8af8846f
Berk Ergün <hasan.ergun@d-teknoloji.com.tr>
2019-08-21 22:07:26 +0300

    Base Completed

---
+++ DrillDownTable/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DrillDownTable.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
